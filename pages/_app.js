ad97833a9f89274acffca424489843e44e6d1fce
Rishabh Sinha <67827763+ris-2107@users.noreply.github.com>
2022-04-23 00:17:49 +0530

    transfer fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,29 @@
-import '../styles/globals.css'
-import  { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { SnackbarProvider } from "notistack";
 
-
-
-const supportedChainsIds = [4]
+const supportedChainsIds = [4];
 const connectors = {
-  injected :{},
-}
-
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-  <ThirdwebWeb3Provider
-  supportedChainsIds={supportedChainsIds}
-  connectors={connectors}
-  >
-   <Component {...pageProps} />
-  </ThirdwebWeb3Provider>
-  )
+    <SnackbarProvider
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "center",
+      }}
+    >
+      <ThirdwebWeb3Provider
+        supportedChainsIds={supportedChainsIds}
+        connectors={connectors}
+      >
+        <Component {...pageProps} />
+      </ThirdwebWeb3Provider>
+    </SnackbarProvider>
+  );
 }
 
-export default MyApp
-
+export default MyApp;
